bd71e534f6d991768e97248800adadd6cf0ebcb7
Michiel <michielvh199@gmail.com>
2015-11-25 11:33:14 +0100

    First Commit
    Lege Windows Form project

---
+++ OMI-ForceDirectedGraph/OMI-ForceDirectedGraph/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace OMI_ForceDirectedGraph
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
