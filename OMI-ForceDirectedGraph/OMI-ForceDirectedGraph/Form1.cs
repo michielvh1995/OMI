28a9af56d18905e87c089c44ebdedd9a7f589616
sjasogun <jessevanderceelen@live.com>
2015-12-05 17:34:03 +0100

    Graphs & verplaatsing
    - Graph generatie geupdatet zodat ze altijd volledig connected zijn
    - Grootste deel van forms1 naar een nieuwe klasse verplaatst vanwege
    organisatie, wat testfuncties staan nog wel in forms1 maar die moeten
    weg als ze niet meer nodig zijn
    - Functie geschreven in Tests.cs die een aantal graphs genereert en ze
    als strings opslaat in een .txt file

--- OMI-ForceDirectedGraph/OMI-ForceDirectedGraph/Form1.cs
+++ OMI-ForceDirectedGraph/OMI-ForceDirectedGraph/Form1.cs
@@ -25,43 +25,8 @@ namespace OMI_ForceDirectedGraph
         static extern bool AllocConsole();
         #endregion
 
-        // The maximum amount of vertices in a graph
-        public const int VerticesAmt = 5;
-
-        public enum AlgorithmType { HookeCoulomb, Eades, FruchtRein };
-
         // Display object for drawing the graphs
         private readonly Display display = new Display();
-        
-        // A total of up to verticesAmt vertices are allowed in the graph
-        internal Vertex[] Vertices = new Vertex[VerticesAmt];
-        private readonly Random rndGen = new Random();
-
-        // Saves all test results as graph qualities indexed by a combination of the used algorithm, the index of the graph used in the graph list and the choices for the constants.
-        // Note that for the Hooke-Coulomb algorithm the third (last) constant value in the key will always be 0
-        private Dictionary<Tuple<AlgorithmType, int, double, double, double>, double> testResults = new Dictionary<Tuple<AlgorithmType, int, double, double, double>, double>();
-
-        // Define the weights for the repulsive and attractive forces
-        private double rWeight = 1;
-        private double aWeight = 1;
-        private double logAWeight = 1;
-        private double FRWeight = 1;
-        private double FRConstant = 1;
-        private double radius = 1;
-
-        private Vector rWeightRange = new Vector(0, 1);
-        private Vector aWeightRange = new Vector(0, 1);
-        private Vector logAWeightRange = new Vector(0, 1);
-        private Vector FRWeightRange = new Vector(0, 1);
-        private Vector FRConstantRange = new Vector(0, 1);
-        private Vector radiusRange = new Vector(0, 1);
-
-        // each run we will have up to a maximum of 100 iterations of calculating and apply force before we stop it
-        private int maxIterations = 100;
-        // We'll try a maximum of 10 settings for each parameter, spread linearly across its range
-        private int maxConstantSettings = 10;
-        // We'll try out the algorithms and algorithm settings on a maximum of 10 distinct graphs
-        private int maxGraphs = 10;
 
         public Form1()
         {
@@ -76,261 +41,45 @@ namespace OMI_ForceDirectedGraph
         {
             // Whether all connections are correct
             bool worker = true;
-            for (int i = 0; i < VerticesAmt; i++)
+            for (int i = 0; i < Tests.VerticesAmt; i++)
             {
-                foreach (int connection in Vertices[i].connectedVertexIDs)
+                foreach (int connection in Tests.Vertices[i].connectedVertexIDs)
                 {
-                    if (!Vertices[connection].ConnectedWith(i))
+                    if (!Tests.Vertices[connection].ConnectedWith(i))
                         worker = false;
                 }
             }
             Console.WriteLine(worker);
 
             int cnt = 0;
-            for (int i = 0; i < VerticesAmt; i++)
+            for (int i = 0; i < Tests.VerticesAmt; i++)
             {
-                for (int j = i + 1; j < VerticesAmt; j++)
+                for (int j = i + 1; j < Tests.VerticesAmt; j++)
                 {
-                    if (Vertices[i].ConnectedWith(j))
+                    if (Tests.Vertices[i].ConnectedWith(j))
                         cnt++;
                 }
             }
             Console.WriteLine(cnt);
 
-            foreach (var v in Vertices)
+            foreach (var v in Tests.Vertices)
             {
                 Console.Write(v.GetConnectionCount() + " ");
             }
             Console.WriteLine();
         }
 
-        // A helper function to linearly interpolate between two values
-        private double lerp(double start, double end, double index)
-        {
-            return (1 - index) * start + index * end;
-        }
-
-        // Conduct the full set of tests for different algorithms, graphs and constant values
-        private void conductTests()
-        {
-            Vertex[][] graphs = new Vertex[maxGraphs][];
-
-            for (int i = 0; i < maxGraphs; i++)
-            {
-                GenerateVertices();
-                graphs[i] = new Vertex[Vertices.Length];
-                this.Vertices.CopyTo(graphs[i], 0);
-            }
-
-            //Hooke-Coulomb algorithm
-            for (int i = 0; i < maxGraphs; i++)
-            {
-                Vertices = graphs[i];
-                for (int j = 0; j < maxConstantSettings; j++)
-                {
-                    aWeight = lerp(aWeightRange.X, aWeightRange.Y, j * (1d / (maxConstantSettings - 1)));
-                    for (int k = 0; k < maxConstantSettings; k++)
-                    {
-                        rWeight = lerp(rWeightRange.X, rWeightRange.Y, k * (1d / (maxConstantSettings - 1)));
-                        for (int l = 0; l < maxIterations; l++)
-                        {
-                            updateForces(AlgorithmType.HookeCoulomb);
-                        }
-                        testResults.Add(Tuple.Create<AlgorithmType, int, double, double, double>(AlgorithmType.HookeCoulomb, i, aWeight, rWeight, 0), QualityTest.qualityTest(Vertices));
-                    }
-                }
-            }
-
-            //Eades algorithm
-            for (int i = 0; i < maxGraphs; i++)
-            {
-                Vertices = graphs[i];
-                for (int j = 0; j < maxConstantSettings; j++)
-                {
-                    aWeight = lerp(aWeightRange.X, aWeightRange.Y, j * (1d / (maxConstantSettings - 1)));
-                    for (int k = 0; k < maxConstantSettings; k++)
-                    {
-                        logAWeight = lerp(logAWeightRange.X, logAWeightRange.Y, k * (1d / (maxConstantSettings - 1)));
-                        for (int l = 0; l < maxIterations; l++)
-                        {
-                            rWeight = lerp(rWeightRange.X, rWeightRange.Y, l * (1d / (maxConstantSettings - 1)));
-                            for (int m = 0; m < maxIterations; m++)
-                            {
-                                updateForces(AlgorithmType.Eades);
-                            }
-                            testResults.Add(Tuple.Create<AlgorithmType, int, double, double, double>(AlgorithmType.Eades, i, aWeight, logAWeight, rWeight), QualityTest.qualityTest(Vertices));
-                        }
-                    }
-                }
-            }
-
-            //Fruchterman-Reingold algorithm
-            for (int i = 0; i < maxGraphs; i++)
-            {
-                Vertices = graphs[i];
-                for (int j = 0; j < maxConstantSettings; j++)
-                {
-                    FRWeight = lerp(FRWeightRange.X, FRWeightRange.Y, j * (1d / (maxConstantSettings - 1)));
-                    for (int k = 0; k < maxConstantSettings; k++)
-                    {
-                        FRConstant = lerp(FRConstantRange.X, FRConstantRange.Y, k * (1d / (maxConstantSettings - 1)));
-                        for (int l = 0; l < maxIterations; l++)
-                        {
-                            radius = lerp(radiusRange.X, radiusRange.Y, l * (1d / (maxConstantSettings - 1)));
-                            for (int m = 0; m < maxIterations; m++)
-                            {
-                                updateForces(AlgorithmType.FruchtRein);
-                            }
-                            testResults.Add(Tuple.Create<AlgorithmType, int, double, double, double>(AlgorithmType.FruchtRein, i, FRWeight, FRConstant, radius), QualityTest.qualityTest(Vertices));
-                        }
-                    }
-                }
-            }
-        }
-
-        // Generate verticesAmt vertices, each with a random position and up to 10 random connections
-        private void GenerateVertices()
-        {
-            for (int i = 0; i < VerticesAmt; i++)
-            {
-                // Random Position
-                int x = rndGen.Next(230, 270);
-                int y = rndGen.Next(230, 270);
-
-                // Random Connections 
-                int connections = 1 + rndGen.Next(2);
-                HashSet<int> connectionSet = new HashSet<int>();
-
-                for (int c = 0; c < connections; c++)
-                {
-                    int conn = rndGen.Next(VerticesAmt);
-                    if (conn != i)
-                        connectionSet.Add(conn);
-                }
-
-                // The ID is its position in the array
-                Vertices[i] = new Vertex(i, new Vector(x, y), connectionSet);
-            }
-
-            // Each of the connections should go both ways.
-            for (int i = 0; i < VerticesAmt; i++)
-            {
-                foreach (int connected in Vertices[i].connectedVertexIDs)
-                {
-                    Vertices[connected].AddConnection(i);
-                }
-            }
-        }
-
-        // General function to calculate the attractive force for each algorithm
-        private Vector attractiveForce(AlgorithmType type, Vertex node1, Vertex node2, double k)
-        {
-            switch (type)
-            {
-                case AlgorithmType.HookeCoulomb:
-                    return Algorithms.HCAttractive(node1, node2, aWeight);
-                    break;
-                case AlgorithmType.Eades:
-                    return Algorithms.EadesAttractive(node1, node2, aWeight, logAWeight);
-                    break;
-                case AlgorithmType.FruchtRein:
-                    return Algorithms.FruchtReinAttractive(node1, node2, k, FRWeight);
-                    break;
-                default:
-                    return new Vector(0, 0);
-            }
-        }
-
-        // General function to calculate the repulsive force for each algorithm
-        private Vector repulsiveForce(AlgorithmType type, Vertex node1, Vertex node2, double k)
-        {
-            switch (type)
-            {
-                case AlgorithmType.HookeCoulomb:
-                    return Algorithms.HCRepulsive(node1, node2, rWeight);
-                    break;
-                case AlgorithmType.Eades:
-                    return Algorithms.EadesRepulsive(node1, node2, rWeight);
-                    break;
-                case AlgorithmType.FruchtRein:
-                    return Algorithms.FruchtReinRepulsive(node1, node2, k, FRWeight);
-                    break;
-                default:
-                    return new Vector(0, 0);
-            }
-        }
-
-        // Main function:
-        private void updateForces(AlgorithmType type)
-        {
-            // Works the same way as array, but can be used concurrently (as we're both reading and writing)
-            var forcesDict = new ConcurrentDictionary<int, Vector>();
-            var closed = new ConcurrentDictionary<int, bool>();
-
-            //Used only when using the FruchtRein algorithm
-            double k = 0;
-
-            // Needs to be instantiated to something
-            for (int i = 0; i < VerticesAmt; i++)
-                forcesDict[i] = new Vector(0, 0);
-
-            for (int i = 0; i < VerticesAmt; i++)
-                closed[i] = false;
-
-            Parallel.For(0, VerticesAmt - 1, i =>
-            {
-                k = type == AlgorithmType.FruchtRein ? Algorithms.FruchtReinConstant(Vertices[i], FRConstant, radius) : 0;
-
-                foreach (int connection in Vertices[i].connectedVertexIDs)
-                {
-                    if (closed[connection])
-                        continue;
-
-                    Vector aForce = attractiveForce(type, Vertices[i], Vertices[connection], k);
-
-                    forcesDict[i] += aForce;
-                }
-
-                closed[i] = true;
-            });
-
-            // Add Repulsive Forces Into the mix:
-            for (int i = 0; i < VerticesAmt; i++)
-                closed[i] = false;
-
-            Parallel.For(0, VerticesAmt - 1, i =>
-            {
-                k = type == AlgorithmType.FruchtRein ? Algorithms.FruchtReinConstant(Vertices[i], FRConstant, radius) : 0;
-
-                for (int j = 0; j < VerticesAmt; j++)
-                {
-                    if (closed[j] || i == j)
-                        continue;
-
-                    Vector rForce = repulsiveForce(type, Vertices[i], Vertices[j], k);
-
-                    forcesDict[i] += rForce;
-                }
-                closed[i] = true;
-            });
-
-            for (int i = 0; i < VerticesAmt; i++)
-            {
-                Vertices[i].ApplyForce(forcesDict[i]);
-            }
-        }
-
         // Displaying the vertices
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            if (Vertices[0] == null) return;
+            if (Tests.Vertices[0] == null) return;
 
-            display.DrawGraph(e.Graphics, Vertices);
+            display.DrawGraph(e.Graphics, Tests.Vertices);
         }
 
         private void GenerateButton_Click(object sender, EventArgs e)
         {
-            this.GenerateVertices();
+            Tests.GenerateVertices(Tests.VerticesAmt);
             pictureBox1.Invalidate();
 
 #if DEBUG   // Testing
@@ -340,16 +89,16 @@ namespace OMI_ForceDirectedGraph
 
         private void ApplyForcesButton_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i < maxIterations; i++)
-                this.updateForces(AlgorithmType.HookeCoulomb);
+            for (int i = 0; i < Tests.maxIterations; i++)
+                Tests.UpdateForces(Tests.AlgorithmType.HookeCoulomb);
             pictureBox1.Invalidate();
 
 #if DEBUG   // Testing
-            for (int i = 0; i < VerticesAmt; i++)
-                Console.WriteLine(Vertices[i].PositionVector);
+            for (int i = 0; i < Tests.VerticesAmt; i++)
+                Console.WriteLine(Tests.Vertices[i].PositionVector);
             Console.WriteLine();
 #endif
-            Console.WriteLine(QualityTest.GetEdgeCrossings(Vertices));
+            Console.WriteLine(QualityTest.GetEdgeCrossings(Tests.Vertices));
         }
     }
 }
---
+++ OMI-ForceDirectedGraph/OMI-ForceDirectedGraph/Tests.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace OMI_ForceDirectedGraph
+{
+    class Tests
+    {
+        #region fields
+        private static readonly Random rndGen = new Random();
+
+        public enum AlgorithmType { HookeCoulomb, Eades, FruchtRein };
+
+        // A total of up to verticesAmt vertices are allowed in the graph
+        internal static Vertex[] Vertices = new Vertex[VerticesAmt];
+
+        // Saves all test results as graph qualities indexed by a combination of the used algorithm, the index of the graph used in the graph list and the choices for the constants.
+        // Note that for the Hooke-Coulomb algorithm the third (last) constant value in the key will always be 0
+        private static Dictionary<Tuple<AlgorithmType, int, double, double, double>, double> testResults = new Dictionary<Tuple<AlgorithmType, int, double, double, double>, double>();
+
+        // Define the weights for the repulsive and attractive forces
+        private static double rWeight = 1;
+        private static double aWeight = 1;
+        private static double logAWeight = 1;
+        private static double FRWeight = 1;
+        private static double FRConstant = 1;
+        private static double radius = 1;
+
+        private static Vector rWeightRange = new Vector(0, 1);
+        private static Vector aWeightRange = new Vector(0, 1);
+        private static Vector logAWeightRange = new Vector(0, 1);
+        private static Vector FRWeightRange = new Vector(0, 1);
+        private static Vector FRConstantRange = new Vector(0, 1);
+        private static Vector radiusRange = new Vector(0, 1);
+
+        // The maximum amount of vertices in a graph
+        public const int VerticesAmt = 5;
+        // each run we will have up to a maximum of 100 iterations of calculating and apply force before we stop it
+        public const int maxIterations = 100;
+        // We'll try a maximum of 10 settings for each parameter, spread linearly across its range
+        public const int maxConstantSettings = 10;
+        // We'll try out the algorithms and algorithm settings on a maximum of 10 distinct graphs
+        public const int maxGraphs = 10;
+        #endregion
+
+        // Generates a number of graphs and writes them to graphs.txt.
+        // One-time use only! These graphs are the ones we'll use for all tests!
+        public static void CreateGraphs(int maxGraphs, int graphSize)
+        {
+            List<string> lines = new List<string>();
+
+            for (int i = 0; i < maxGraphs; i++)
+            {
+                lines = lines.Concat(GraphToStrings(GenerateVertices(graphSize))).ToList();
+                lines.Add("");
+            }
+            lines.RemoveAt(lines.Count - 1);
+
+            File.WriteAllLines(Directory.GetCurrentDirectory() + @"\graphs.txt", lines);
+        }
+
+        // Generates a specified number of vertices and their connections
+        // Guaranteed is that the graph that these vertices form will be completely connected,
+        // ie every vertex can be directly or indirectly reached by every other vertex
+        public static Vertex[] GenerateVertices(int amount)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                // Random Position
+                int x = rndGen.Next(230, 270);
+                int y = rndGen.Next(230, 270);
+
+                // Random Connections 
+                int connections = rndGen.Next(2);
+                HashSet<int> connectionSet = new HashSet<int>();
+                HashSet<int> connectedVertices = new HashSet<int>();
+
+                int connection;
+                do
+                { 
+                    connection = connectedVertices.Count > 0 ? connectedVertices.ToList()[rndGen.Next(connectedVertices.Count)] : rndGen.Next(amount);
+                } while (connection == i);
+                connectionSet.Add(connection);
+                connectedVertices.Add(i);
+
+                for (int c = 0; c < connections; c++)
+                {
+                    int conn = rndGen.Next(amount);
+                    if (conn != i)
+                    {
+                        connectionSet.Add(conn);
+                        connectedVertices.Add(i);
+                        connectedVertices.Add(conn);
+                    }
+                }
+
+                // The ID is its position in the array
+                Vertices[i] = new Vertex(i, new Vector(x, y), connectionSet);
+            }
+
+            // Each of the connections should go both ways.
+            for (int i = 0; i < amount; i++)
+            {
+                foreach (int connected in Vertices[i].connectedVertexIDs)
+                {
+                    Vertices[connected].AddConnection(i);
+                }
+            }
+
+            return Vertices;
+        }
+
+        // Converts a list of vertices to a list of strings
+        // Helper function for CreateGraphs
+        private static List<string> GraphToStrings(Vertex[] vertices)
+        {
+            List<string> lines = new List<string>();
+            string id, x, y, connections;
+
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                id = vertices[i].ID.ToString();
+                x = vertices[i].PositionVector.X.ToString();
+                y = vertices[i].PositionVector.Y.ToString();
+                connections = string.Join(",", vertices[i].connectedVertexIDs.ToArray());
+
+                lines.Add(id + " " + x + " " + y + " " + connections);
+            }
+
+            return lines;
+        }
+
+        // Conduct the full set of tests for different algorithms, graphs and constant values
+        public static void ConductTests()
+        {
+            Vertex[][] graphs = new Vertex[maxGraphs][];
+
+            for (int i = 0; i < maxGraphs; i++)
+                graphs[i] = GenerateVertices(VerticesAmt);
+
+            //Hooke-Coulomb algorithm
+            for (int i = 0; i < maxGraphs; i++)
+            {
+                Vertices = graphs[i];
+                for (int j = 0; j < maxConstantSettings; j++)
+                {
+                    aWeight = Lerp(aWeightRange.X, aWeightRange.Y, j * (1d / (maxConstantSettings - 1)));
+                    for (int k = 0; k < maxConstantSettings; k++)
+                    {
+                        rWeight = Lerp(rWeightRange.X, rWeightRange.Y, k * (1d / (maxConstantSettings - 1)));
+                        for (int l = 0; l < maxIterations; l++)
+                        {
+                            UpdateForces(AlgorithmType.HookeCoulomb);
+                        }
+                        testResults.Add(Tuple.Create<AlgorithmType, int, double, double, double>(AlgorithmType.HookeCoulomb, i, aWeight, rWeight, 0), QualityTest.qualityTest(Vertices));
+                    }
+                }
+            }
+
+            //Eades algorithm
+            for (int i = 0; i < maxGraphs; i++)
+            {
+                Vertices = graphs[i];
+                for (int j = 0; j < maxConstantSettings; j++)
+                {
+                    aWeight = Lerp(aWeightRange.X, aWeightRange.Y, j * (1d / (maxConstantSettings - 1)));
+                    for (int k = 0; k < maxConstantSettings; k++)
+                    {
+                        logAWeight = Lerp(logAWeightRange.X, logAWeightRange.Y, k * (1d / (maxConstantSettings - 1)));
+                        for (int l = 0; l < maxIterations; l++)
+                        {
+                            rWeight = Lerp(rWeightRange.X, rWeightRange.Y, l * (1d / (maxConstantSettings - 1)));
+                            for (int m = 0; m < maxIterations; m++)
+                            {
+                                UpdateForces(AlgorithmType.Eades);
+                            }
+                            testResults.Add(Tuple.Create<AlgorithmType, int, double, double, double>(AlgorithmType.Eades, i, aWeight, logAWeight, rWeight), QualityTest.qualityTest(Vertices));
+                        }
+                    }
+                }
+            }
+
+            //Fruchterman-Reingold algorithm
+            for (int i = 0; i < maxGraphs; i++)
+            {
+                Vertices = graphs[i];
+                for (int j = 0; j < maxConstantSettings; j++)
+                {
+                    FRWeight = Lerp(FRWeightRange.X, FRWeightRange.Y, j * (1d / (maxConstantSettings - 1)));
+                    for (int k = 0; k < maxConstantSettings; k++)
+                    {
+                        FRConstant = Lerp(FRConstantRange.X, FRConstantRange.Y, k * (1d / (maxConstantSettings - 1)));
+                        for (int l = 0; l < maxIterations; l++)
+                        {
+                            radius = Lerp(radiusRange.X, radiusRange.Y, l * (1d / (maxConstantSettings - 1)));
+                            for (int m = 0; m < maxIterations; m++)
+                            {
+                                UpdateForces(AlgorithmType.FruchtRein);
+                            }
+                            testResults.Add(Tuple.Create<AlgorithmType, int, double, double, double>(AlgorithmType.FruchtRein, i, FRWeight, FRConstant, radius), QualityTest.qualityTest(Vertices));
+                        }
+                    }
+                }
+            }
+        }
+
+        // A helper function to linearly interpolate between two values
+        private static double Lerp(double start, double end, double index)
+        {
+            return (1 - index) * start + index * end;
+        }
+
+        // General function to calculate the attractive force for each algorithm
+        private static Vector AttractiveForce(AlgorithmType type, Vertex node1, Vertex node2, double k)
+        {
+            switch (type)
+            {
+                case AlgorithmType.HookeCoulomb:
+                    return Algorithms.HCAttractive(node1, node2, aWeight);
+                case AlgorithmType.Eades:
+                    return Algorithms.EadesAttractive(node1, node2, aWeight, logAWeight);
+                case AlgorithmType.FruchtRein:
+                    return Algorithms.FruchtReinAttractive(node1, node2, k, FRWeight);
+                default:
+                    return new Vector(0, 0);
+            }
+        }
+
+        // General function to calculate the repulsive force for each algorithm
+        private static Vector RepulsiveForce(AlgorithmType type, Vertex node1, Vertex node2, double k)
+        {
+            switch (type)
+            {
+                case AlgorithmType.HookeCoulomb:
+                    return Algorithms.HCRepulsive(node1, node2, rWeight);
+                case AlgorithmType.Eades:
+                    return Algorithms.EadesRepulsive(node1, node2, rWeight);
+                case AlgorithmType.FruchtRein:
+                    return Algorithms.FruchtReinRepulsive(node1, node2, k, FRWeight);
+                default:
+                    return new Vector(0, 0);
+            }
+        }
+
+        // Main function:
+        public static void UpdateForces(AlgorithmType type)
+        {
+            // Works the same way as array, but can be used concurrently (as we're both reading and writing)
+            var forcesDict = new ConcurrentDictionary<int, Vector>();
+            var closed = new ConcurrentDictionary<int, bool>();
+
+            //Used only when using the FruchtRein algorithm
+            double k = 0;
+
+            // Needs to be instantiated to something
+            for (int i = 0; i < VerticesAmt; i++)
+                forcesDict[i] = new Vector(0, 0);
+
+            for (int i = 0; i < VerticesAmt; i++)
+                closed[i] = false;
+
+            Parallel.For(0, VerticesAmt - 1, i =>
+            {
+                k = type == AlgorithmType.FruchtRein ? Algorithms.FruchtReinConstant(Vertices[i], FRConstant, radius) : 0;
+
+                foreach (int connection in Vertices[i].connectedVertexIDs)
+                {
+                    if (closed[connection])
+                        continue;
+
+                    Vector aForce = AttractiveForce(type, Vertices[i], Vertices[connection], k);
+
+                    forcesDict[i] += aForce;
+                }
+
+                closed[i] = true;
+            });
+
+            // Add Repulsive Forces Into the mix:
+            for (int i = 0; i < VerticesAmt; i++)
+                closed[i] = false;
+
+            Parallel.For(0, VerticesAmt - 1, i =>
+            {
+                k = type == AlgorithmType.FruchtRein ? Algorithms.FruchtReinConstant(Vertices[i], FRConstant, radius) : 0;
+
+                for (int j = 0; j < VerticesAmt; j++)
+                {
+                    if (closed[j] || i == j)
+                        continue;
+
+                    Vector rForce = RepulsiveForce(type, Vertices[i], Vertices[j], k);
+
+                    forcesDict[i] += rForce;
+                }
+                closed[i] = true;
+            });
+
+            for (int i = 0; i < VerticesAmt; i++)
+            {
+                Vertices[i].ApplyForce(forcesDict[i]);
+            }
+        }
+    }
+}
